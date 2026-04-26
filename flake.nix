94b010f6e8c0c1b1e29a1992577305e1b79e30e3
Miscere <misccorg@gmail.com>
2026-04-26 22:54:56 +0300

    поставил хоум

--- flake.nix
+++ flake.nix
@@ -12,3 +12,29 @@
     };
   };
 }
+
+{
+  description = "Мой NixOS флейк";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    # Добавляем Home Manager
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.miscere = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
