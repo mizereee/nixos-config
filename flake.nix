88070cde20a52a5039bcd02efb5825cf16fc1d3b
Miscere <misccorg@gmail.com>
2026-04-26 14:56:53 +0300

    Добавил все файлы конфигурации

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Мой NixOS флейк";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
