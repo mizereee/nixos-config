74be911835e68cb0e048f24b63648b2f3930ab1d
Miscere <misccorg@gmail.com>
2026-04-26 22:59:34 +0300

    ne typanyl

--- flake.nix
+++ flake.nix
@@ -3,22 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-  };
-
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-    };
-  };
-}
-
-{
-  description = "Мой NixOS флейк";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    # Добавляем Home Manager
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
