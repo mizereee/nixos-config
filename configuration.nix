eefc63ac2f85f08f69ee76f022aec62f9c1abd55
mizereee <misccorg@gmail.com>
2026-05-02 23:17:14 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./system/system.nix
-      ./system/drova.nix
+#      ./system/drova.nix
       ./system/core.nix
     ];
 
